23cf464984e3b70e3ef5e63b930dae5b2fe09ce4
Zachary <zacharytay1994@gmail.com>
2021-12-30 15:35:55 +0800

    Image Views

--- VulkanBasics/main.cpp
+++ VulkanBasics/main.cpp
@@ -116,6 +116,7 @@ private:
     std::vector<VkImage> swapChainImages;
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
+    std::vector<VkImageView> swapChainImageViews;
 
     // vulkan sdk validation layers
     const std::vector<const char*> validationLayers = {
@@ -277,6 +278,35 @@ private:
         pickPhysicalDevice();
         createLogicalDevice ();
         createSwapChain ();
+        createImageViews ();
+    }
+
+    void createImageViews ()
+    {
+        swapChainImageViews.resize ( swapChainImages.size () );
+
+        for ( size_t i = 0; i < swapChainImages.size (); ++i )
+        {
+            VkImageViewCreateInfo createInfo {};
+            createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+            createInfo.image = swapChainImages[ i ];
+            createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+            createInfo.format = swapChainImageFormat;
+            createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+            createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+            createInfo.subresourceRange.baseMipLevel = 0;
+            createInfo.subresourceRange.levelCount = 1;
+            createInfo.subresourceRange.baseArrayLayer = 0;
+            createInfo.subresourceRange.layerCount = 1;
+
+            if ( vkCreateImageView ( device , &createInfo , nullptr , &swapChainImageViews[ i ] ) != VK_SUCCESS )
+            {
+                throw std::runtime_error ( "failed to create image views!" );
+            }
+        }
     }
 
     void createSwapChain ()
@@ -558,6 +588,12 @@ private:
 
     void cleanup()
     {
+        // clean up image views created by us
+        for ( auto imageView : swapChainImageViews )
+        {
+            vkDestroyImageView ( device , imageView , nullptr );
+        }
+
         // cleanup swap chain before device
         vkDestroySwapchainKHR ( device , swapChain , nullptr );
 
