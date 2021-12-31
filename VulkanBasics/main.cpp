da6372d844121ea0f065205ca92c9216199f239a
Zachary <zacharytay1994@gmail.com>
2021-12-31 17:44:34 +0800

    Render Pass

--- VulkanBasics/main.cpp
+++ VulkanBasics/main.cpp
@@ -148,6 +148,7 @@ private:
     VkFormat swapChainImageFormat;
     VkExtent2D swapChainExtent;
     std::vector<VkImageView> swapChainImageViews;
+    VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
 
     // vulkan sdk validation layers
@@ -311,7 +312,45 @@ private:
         createLogicalDevice ();
         createSwapChain ();
         createImageViews ();
+        createRenderPass ();
         createGraphicsPipeline ();
+    }
+
+    void createRenderPass ()
+    {
+        // single color buffer attachment from one of the images from the swap chain
+        VkAttachmentDescription colorAttachment {};
+        colorAttachment.format = swapChainImageFormat;
+        colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
+        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+        // subpasses and attachment references, for postprocessing
+        VkAttachmentReference colorAttachmentRef {};
+        colorAttachmentRef.attachment = 0;
+        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+        VkSubpassDescription subpass {};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &colorAttachmentRef;
+
+        // create render pass
+        VkRenderPassCreateInfo renderPassInfo {};
+        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        renderPassInfo.attachmentCount = 1;
+        renderPassInfo.pAttachments = &colorAttachment;
+        renderPassInfo.subpassCount = 1;
+        renderPassInfo.pSubpasses = &subpass;
+
+        if ( vkCreateRenderPass ( device , &renderPassInfo , nullptr , &renderPass ) != VK_SUCCESS )
+        {
+            throw std::runtime_error ( "failed to create render pass!" );
+        }
     }
 
     VkShaderModule createShaderModule ( std::vector<char> const& code )
@@ -793,6 +832,7 @@ private:
     {
         // clean up pipeline layout
         vkDestroyPipelineLayout ( device , pipelineLayout , nullptr );
+        vkDestroyRenderPass ( device , renderPass , nullptr );
 
         // clean up image views created by us
         for ( auto imageView : swapChainImageViews )
