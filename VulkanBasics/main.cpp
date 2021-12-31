14b41b79b9bec94d9b1414dd21ea516ae26df313
Zachary <zacharytay1994@gmail.com>
2021-12-31 21:28:09 +0800

    Command Buffers

--- VulkanBasics/main.cpp
+++ VulkanBasics/main.cpp
@@ -152,6 +152,8 @@ private:
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
     std::vector<VkFramebuffer> swapChainFramebuffers;
+    VkCommandPool commandPool;
+    std::vector<VkCommandBuffer> commandBuffers;
 
     // vulkan sdk validation layers
     const std::vector<const char*> validationLayers = {
@@ -317,6 +319,88 @@ private:
         createRenderPass ();
         createGraphicsPipeline ();
         createFramebuffers ();
+        createCommandPool ();
+        createCommandBuffers ();
+    }
+
+    void createCommandBuffers ()
+    {
+        commandBuffers.resize ( swapChainFramebuffers.size () );
+
+        VkCommandBufferAllocateInfo allocInfo {};
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.commandPool = commandPool;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+        allocInfo.commandBufferCount = ( uint32_t ) commandBuffers.size ();
+
+        if ( vkAllocateCommandBuffers ( device , &allocInfo , commandBuffers.data () ) != VK_SUCCESS )
+        {
+            throw std::runtime_error ( "failed to allocate command buffers!" );
+        }
+
+        for ( size_t i = 0; i < commandBuffers.size (); ++i )
+        {
+            // begin command buffer
+            VkCommandBufferBeginInfo beginInfo {};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+            beginInfo.flags = 0;
+            beginInfo.pInheritanceInfo = nullptr;
+
+            if ( vkBeginCommandBuffer ( commandBuffers[ i ] , &beginInfo ) != VK_SUCCESS )
+            {
+                throw std::runtime_error ( "failed to begin recording command buffer!" );
+            }
+
+            // assign render pass to command buffer and begin render pass
+            VkRenderPassBeginInfo renderPassInfo {};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = renderPass;
+            renderPassInfo.framebuffer = swapChainFramebuffers[ i ];
+            renderPassInfo.renderArea.offset = { 0,0 };
+            renderPassInfo.renderArea.extent = swapChainExtent;
+
+            VkClearValue clearColor = { {{0.0f, 0.0f, 0.0f, 1.0f}} };
+            renderPassInfo.clearValueCount = 1;
+            renderPassInfo.pClearValues = &clearColor;
+
+            vkCmdBeginRenderPass ( commandBuffers[ i ] , &renderPassInfo , VK_SUBPASS_CONTENTS_INLINE );
+
+            // bind graphics pipeline
+            vkCmdBindPipeline ( commandBuffers[ i ] , VK_PIPELINE_BIND_POINT_GRAPHICS , graphicsPipeline );
+
+            // bind draw command
+            // param
+            // 1. command buffer
+            // 2. vertex count
+            // 3. first vertex
+            // 4. first instance
+            vkCmdDraw ( commandBuffers[ i ] , 3 , 1 , 0 , 0 );
+
+            // end render pass
+            vkCmdEndRenderPass ( commandBuffers[ i ] );
+
+            // end command buffer
+            if ( vkEndCommandBuffer ( commandBuffers[ i ] ) != VK_SUCCESS )
+            {
+                throw std::runtime_error ( "failed to record command buffer!" );
+            }
+        }
+    }
+
+    // command pool stores draw commands
+    void createCommandPool ()
+    {
+        QueueFamilyIndices queueFamilyIndices = findQueueFamilies ( physicalDevice );
+        
+        VkCommandPoolCreateInfo poolInfo {};
+        poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+        poolInfo.queueFamilyIndex = queueFamilyIndices.graphicsFamily.value ();
+        poolInfo.flags = 0;
+
+        if ( vkCreateCommandPool ( device , &poolInfo , nullptr , &commandPool ) != VK_SUCCESS )
+        {
+            throw std::runtime_error ( "failed to create command pool!" );
+        }
     }
 
     void createFramebuffers ()
@@ -894,6 +978,9 @@ private:
 
     void cleanup()
     {
+        // clean up command pool
+        vkDestroyCommandPool ( device , commandPool , nullptr );
+
         // clean up framebuffers
         for ( auto framebuffer : swapChainFramebuffers )
         {
